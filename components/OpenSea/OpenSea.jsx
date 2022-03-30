8088369e3b55e1e31faa055759e86a3358f39baa
Jahid Hasan <jahidhasan2856@gmail.com>
2022-03-30 17:48:10 +0600

    opensea image added

--- components/OpenSea/OpenSea.jsx
+++ components/OpenSea/OpenSea.jsx
@@ -1,10 +1,14 @@
+import Image from "next/image";
 import React from "react";
+import dolphin from "../../public/img/flow-shark.png";
 import OpenSlider from "./OpenSlider";
-
 const OpenSea = () => {
   return (
     <>
-      <section className="top-section py-24">
+      <section className="top-section py-24 relative">
+        <div className="absolute lg:-top-[9rem] lg:left-[20rem] -top-[8rem] ">
+          <Image width={350} height={350} src={dolphin} alt="" />
+        </div>
         <div className="section-title w-9/12 mx-auto md:flex space-y-4 justify-between items-center ">
           <button className="text-base bg-lime-500 text-white rounded-lg px-12 py-3 shadow-lg mt-8">
             View On Opensea
